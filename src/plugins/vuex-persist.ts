49429b4d132998ff4e7f61e1d6beb6ab777f8d5a
DeltaByte <thedeltabyte@gmail.com>
2022-11-28 03:59:20 +0000

    refactor: prefix vuex localStorage

--- src/plugins/vuex-persist.ts
+++ src/plugins/vuex-persist.ts
@@ -1,12 +1,13 @@
 import { Plugin } from '@nuxt/types'
-import VuexPersistence from 'vuex-persist'
+import { VuexPersistence } from 'vuex-persist'
 
 const persistedStores = ['domains']
+const keyPrefix = 'littleurl.'
 
 const vuexPersistPlugin: Plugin = ({ store }) => {
   for (const persistedStore of persistedStores) {
     new VuexPersistence({
-      key: persistedStore,
+      key: keyPrefix + persistedStore,
       modules: [persistedStore],
     }).plugin(store)
   }
